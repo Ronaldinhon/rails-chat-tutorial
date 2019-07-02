6e80763835d0fe8ba565cc3cf010ff541862b0f3
Ronaldinhon <stanleychoo0522@hotmail.com>
2019-07-02 09:14:56 +0800

    Created ReplyService

--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -5,8 +5,8 @@ class RoomMessagesController < ApplicationController
     @room_message = RoomMessage.create user: current_user,
                                        room: @room,
                                        message: params.dig(:room_message, :message)
-
-   RoomChannel.broadcast_to @room, @room_message
+    ReplyService.new(params.dig(:room_message, :message)).call                                 
+    RoomChannel.broadcast_to @room, @room_message
   end
 
   protected
---
+++ app/services/reply_service.rb
@@ -0,0 +1,10 @@
+require 'httparty'
+class ReplyService
+	def initialize(message)
+    @message = message
+  end
+
+  def call
+  	byebug
+  end
+end
